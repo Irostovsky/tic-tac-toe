b35424aa42809cb97b21a0c94c3c586d00bf6ad8
Ivan Rostovsky <ivan.rostovsky@gmail.com>
2023-11-06 20:06:55 +0200

    Show logs on the page.

---
+++ src/components/Log.jsx
@@ -0,0 +1,16 @@
+const Log = ({ turns }) => {
+  return (
+    <ol id="log">
+      {turns.map(({ square, player }) => {
+        const { row, col } = square;
+        return (
+          <li key={`${row}${col}`}>
+            {player} selected {row},{col}
+          </li>
+        );
+      })}
+    </ol>
+  );
+};
+
+export default Log;
